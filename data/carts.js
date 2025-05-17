37beb80c37cf2a8a2238f65bcf78dde0ab4c1e72
PiaCarlos <carlos.andres.rosoha.alvarado@gmail.com>
2025-05-16 21:34:06 -0400

    add/removes products now work

--- data/carts.js
+++ data/carts.js
@@ -1,15 +1,24 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 }, {
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
+}]
 }
-];
 
-export let cartQuantity = 0; 
+
+// save to storage
+function savetoStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
 
 // function to add items to cart and updates cart quantity
+export let cartQuantity = 0; 
 export function addToCart(productId) {
   let matchingItem; 
     // add items to the cart - for now only one at a time
@@ -30,6 +39,7 @@ export function addToCart(productId) {
         });
       cartQuantity += 1;
     }
+    savetoStorage();
 }
 
 
@@ -37,4 +47,5 @@ export function removeFromCart(productId) {
     // remove the item
     let cartNew = [];
     cart = cart.filter(item => item.productId !== productId);
-  };
+    savetoStorage();
+  }
